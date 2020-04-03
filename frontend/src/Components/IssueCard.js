e18bf75536f3bc3815b5964da72fed42946eebee
Brian Norman <b2norman@uwaterloo.ca>
2020-04-03 16:25:39 -0600

    Added notifications

--- frontend/src/Components/IssueCard.js
+++ frontend/src/Components/IssueCard.js
@@ -1,17 +1,21 @@
 import React from "react"
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import Badge from 'react-bootstrap/Badge'
 
 export default function IssueCard(props) {
     // TODO: Get user who posted profile photo to display
     // TODO: Notification for new issue
     return (
-        <Card style={{ width: '25rem', marginTop: '1rem', marginLeft: '1rem' }}>
+        <div>
+        <Badge pill variant= "light">{props.notif ? "🔔" : ""}</Badge>
+        <Card style={{ width: '25rem', marginBottom: '1rem', marginLeft: '1rem' }}>
             <Card.Body>
                 <Card.Title>{props.title}</Card.Title>
                 <Card.Text> {props.date} - {props.user}</Card.Text>
                 <Button variant="outline-primary" style={{position: "absolute", right: "0.5rem", bottom: "0.5rem"}}>Open</Button>
             </Card.Body>
         </Card>
+        </div>
     )
 }
