1d1c37ccdd84397fd699121574fcb1ff832a384f
Zahin Mohammad <zahin.dev@gmail.com>
2020-04-03 16:53:38 -0400

    got the server working

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ connection.connect()
 connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
     if (err) throw err
 
-    console.log('The solution is: ', rows[0].solution)
+    console.log(rows[0].solution)
 })
 
 const app = express()
