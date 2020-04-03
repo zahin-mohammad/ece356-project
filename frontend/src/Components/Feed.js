ca4fbf8be09d81d98cb77fd35b2c52ff0b1598f6
Brian Norman <b2norman@uwaterloo.ca>
2020-04-03 15:06:05 -0600

    Using React-Bootstrap

--- frontend/src/Components/Feed.js
+++ frontend/src/Components/Feed.js
@@ -4,7 +4,6 @@ import IssueCard from "./IssueCard.js"
 export default function Feed() {
     return (
         <div>
-            <h1>Feed</h1>
             <IssueCard />
             <IssueCard />
             <IssueCard />
--- frontend/src/Components/IssueCard.js
+++ frontend/src/Components/IssueCard.js
@@ -1,7 +1,15 @@
 import React from "react"
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 export default function IssueCard() {
     return (
-        <h4>Issue Title</h4>
+        <Card style={{ width: '18rem', marginTop: '1rem', marginLeft: '1rem' }}>
+            <Card.Body>
+                <Card.Title>Issue Title</Card.Title>
+                <Card.Text> The body of the first comment on the issue</Card.Text>
+                <Button variant="primary">Open</Button>
+            </Card.Body>
+        </Card>
     )
 }
