83052c264a7b861b790df54cb61f02e5eddc7f88
Brian Norman <b2norman@uwaterloo.ca>
2020-04-03 15:38:10 -0600

    Using props

--- frontend/src/Components/IssueCard.js
+++ frontend/src/Components/IssueCard.js
@@ -2,12 +2,13 @@ import React from "react"
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
-export default function IssueCard() {
+export default function IssueCard(props) {
+    // TODO: Get user who posted profile photo to display
     return (
-        <Card style={{ width: '18rem', marginTop: '1rem', marginLeft: '1rem' }}>
+        <Card style={{ width: '25rem', marginTop: '1rem', marginLeft: '1rem' }}>
             <Card.Body>
-                <Card.Title>Issue Title</Card.Title>
-                <Card.Text> The body of the first comment on the issue</Card.Text>
+                <Card.Title>{props.title}</Card.Title>
+                <Card.Text> {props.date} - {props.user}</Card.Text>
                 <Button variant="primary">Open</Button>
             </Card.Body>
         </Card>
