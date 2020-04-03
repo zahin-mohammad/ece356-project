47a7d3069aefb77bcd54c23436131742d0f9d5d2
Brian Norman <b2norman@uwaterloo.ca>
2020-04-03 17:24:13 -0600

    Less hardcoding for users

--- frontend/src/Components/UserCard.js
+++ frontend/src/Components/UserCard.js
@@ -4,10 +4,11 @@ import Button from 'react-bootstrap/Button'
 
 export default function UserCard(props) {
     return (
-        <Card style={{ width: '12rem', height: '20rem', marginTop: '1rem', marginLeft: '1rem' }}>
-            <Card.Img variant="top" src={props.pictureUrl} />
+        <Card style={{ width: '14rem', height: '24rem', marginTop: '1rem', marginLeft: '1rem' }}>
+            <Card.Img variant="top" src={props.avatar_url} />
             <Card.Body>
                 <Card.Title>{props.name}</Card.Title>
+                <Card.Text>{props.email}</Card.Text>
                 <Button variant="outline-primary" style={{position: "absolute", right: "0.5rem", bottom: "0.5rem"}}>Unfollow</Button>
             </Card.Body>
         </Card>
