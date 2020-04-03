1de58ed47304eac7b38e1379ff15294cb915ab23
Brian Norman <b2norman@uwaterloo.ca>
2020-04-03 13:07:48 -0600

    Initializing react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
-import React from 'react'
-import './App.css'
+import React from "react"
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom"
+import SignOut from "./Components/SignOut"
+import Home from "./Components/Home"
+
 
 export default function App() {
+  // TODO: https://reacttraining.com/react-router/web/example/auth-workflow
+  let navStyle = {
+    fontWeight: "normal",
+    margin: "0.5rem",
+    background: "none"
+  }
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <NavLink exact to = "/" style = {navStyle}> Home </NavLink>
+          <NavLink exact to = "/signOut" style = {navStyle}> Sign Out </NavLink>
+        </nav>
+
+        <Switch>
+          <Route path = "/signOut">
+            <SignOut />
+          </Route>
+          <Route path = "/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
