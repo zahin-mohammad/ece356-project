7c269d2708879389f131329b4baf619dd4b4b68c
Zahin Mohammad <zahin.dev@gmail.com>
2020-04-04 13:17:44 -0400

    change import

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,26 +1,26 @@
 import React, { useContext } from "react"
 import { AuthContext } from "../App"
 import Button from "react-bootstrap/Button"
-import Navbar from "react-bootstrap/NavBar"
+import Navbar from "react-bootstrap/Navbar"
 
 
 export default function Header() {
-    const {state, dispatch} = useContext(AuthContext)
+    const { state, dispatch } = useContext(AuthContext)
 
     return (
         <Navbar bg="dark" variant="dark" fixed="top">
             <Navbar.Brand href="/">Home</Navbar.Brand>
-                { state.isAuthenticated ? 
+            {state.isAuthenticated ?
                 <Button
-                    style = {{ position: "absolute", right: "1rem" }}
-                    variant="light" 
+                    style={{ position: "absolute", right: "1rem" }}
+                    variant="light"
                     onClick={() =>
                         dispatch({
                             type: "LOGOUT"
                         })
                     }
                 > Sign Out
-                </Button> : null }
+                </Button> : null}
         </Navbar>
     )
 }
